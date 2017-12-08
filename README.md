979850205b668250a24ef4be53119cd67c4af71d
Christopher Yovanovitch <yovano_c@outlook.com>
2017-12-08 13:20:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# AsData
+
+Extended ActionScript3 ByteArray API for JavaScript
+
+You need **Yarn** in order to use this project properly.
+```sh
+npm install -g yarn
+```
---
+++ tests/bytearray.test.ts
@@ -0,0 +1,36 @@
+import * as chai from "chai";
+import * as mocha from "mocha";
+import ByteArray from "../src";
+
+const expect = chai.expect;
+
+let ba: ByteArray;
+
+beforeEach((done) => {
+  ba = new ByteArray();
+  done();
+});
+
+describe("ByteArray", () => {
+  it("should create an instance of ByteArray", () => {
+    ba = new ByteArray();
+  });
+
+  it("should write and read a string", () => {
+    ba.writeUTF("Hello!");
+    ba.position = 0;
+    expect(ba.readUTF()).eq("Hello!");
+  });
+
+  it("should write and read a number", () => {
+    ba.writeInt(1234);
+    ba.position = 0;
+    expect(ba.readInt()).eq(1234);
+  });
+
+  it("should write and read a boolean", () => {
+    ba.writeBoolean(true);
+    ba.position = 0;
+    expect(ba.readBoolean()).eq(true);
+  });
+});
